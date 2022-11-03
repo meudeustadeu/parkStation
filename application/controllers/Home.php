4b0c389dbde1e02748a2d5c6b5e1ff51ceeb58f2
Tadeu Luiz Carvaho Miranda <tadeu.carvalho@expressotecnologia.com>
2022-11-03 14:06:22 -0300

    layout insert

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,13 @@ defined ('BASEPATH') or exit ('Ação não permitida');
 class Home extends CI_Controller{
 
     public function index(){
+        $data = array(
+            'titulo' => 'Home'
+        );
+
+        $this->load->view('layout/header', $data);
         $this->load->view('home/index');
+        $this->load->view('layout/footer');
 
     }
 
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,21 @@
-<h1>Você está na Home, Bem- vindo!</h1>
+        <?php $this->load->view('layout/navbar')?>
+
+        <div class="page-wrap">
+
+        <?php $this->load->view('layout/sidebar') ?>
+
+            <div class="main-content">
+                <div class="container-fluid">
 
 
+
+                </div>
+            </div>
+
+            <footer class="footer">
+                <div class="w-100 clearfix">
+                    <span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo date('Y') ?> ThemeKit v2.0. All Rights Reserved.</span>
+                    <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Customization <i class="fa fa-code text-dark"></i> by <a href="javascript:void" class="text-dark">dev@meudeustadeu</a></span>
+                </div>
+            </footer>
+        </div>
